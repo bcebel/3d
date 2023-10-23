a818f761e72b60e59b0d6e7f749a5e772ce041ab
bcebel <bcebel@gmail.com>
2023-10-23 10:32:28 -0400

    console

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
         function handleOrientation(event) {
             const compassDirection = event.alpha;
-
+            console.log(compassDirection);
             if (compassDirection !== null) {
                 // Update the DOM element with the compass direction value
                 document.getElementById('compassDirection').innerText = `Compass Direction: ${compassDirection}°`;
