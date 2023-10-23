22650ec8f0926af2ead94e531f5344de0a5e6429
bcebel <bcebel@gmail.com>
2023-10-23 14:07:50 -0400

    didn't like amp

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 console.log("this");
 const p = document.createElement("p");
 
+window.addEventListener("deviceorientation", handleOrientation);
 
-
-       window.addEventListener('deviceorientationabsolute', handleOrientation);
-
-        function handleOrientation(event) {
-            const compassDirection = event.alpha;
-            console.log(compassDirection);
-            if (compassDirection !== null) {
-                // Update the DOM element with the compass direction value
-                document.getElementById('compassDirection').innerText = `Compass Direction: ${compassDirection}°`;
-            }
-        }
-        p.textContent = "A second line of text";
-        document.body.appendChild(p);
+function handleOrientation(event) {
+  const compassDirection = event.alpha;
+  console.log(compassDirection);
+  if (compassDirection !== null) {
+    // Update the DOM element with the compass direction value
+    document.getElementById(
+      "compassDirection"
+    ).innerText = `Compass Direction: ${compassDirection}°`;
+  }
+}
+p.textContent = "A second line of text";
+document.body.appendChild(p);
