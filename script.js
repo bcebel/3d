81419c2c6866b39302599961bedd4d4d8331a80c
bcebel <bcebel@gmail.com>
2023-10-23 11:24:12 -0400

    test

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 console.log("this");
-const p = document.createElement("p");
+
 
        window.addEventListener('deviceorientationabsolute', handleOrientation);
 
         function handleOrientation(event) {
             const compassDirection = event.alpha;
             console.log(compassDirection);
+            const p = document.createElement("p");
         p.textContent = compassDirection;
         document.body.appendChild(p);
         }
