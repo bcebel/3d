fe542e28a07c6299f38a52c3e7f38a617dfbd55e
bcebel <bcebel@gmail.com>
2023-10-23 15:38:06 -0400

    test

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ console.log("this");
 window.addEventListener("deviceorientationabsolute", handleOrientation);
 
 function handleOrientation(event) {
-    const compassDirection = 360-event.alpha;
-    <template type="amp-mustache">{{ compassDirection }}</template>;
+  //    const compassDirection = 360-event.alpha;
+  const compassDirection = 360 - event.alpha;
+  <template type="amp-mustache">{{ compassDirection }}</template>;
   console.log(compassDirection);
   if (compassDirection !== null) {
     // Update the DOM element with the compass direction value
-    document.getElementById(
-      "compassDirection"
-    ).innerText = `Compass Direction: ${compassDirection}°`;
+    document.getElementById("inputCompass").setAttribute =
+      (heading - end, compassDirection);
+    console.log(inputCompass.compassDirection);
   }
 }
-
