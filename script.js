cb4c651b99f69f154155e5c69e2e18a8bf7ffc4e
bcebel <bcebel@gmail.com>
2023-10-23 11:20:40 -0400

    test of element

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 console.log("this");
+const p = document.createElement("p");
+
        window.addEventListener('deviceorientationabsolute', handleOrientation);
 
         function handleOrientation(event) {
             const compassDirection = event.alpha;
             console.log(compassDirection);
-            if (compassDirection !== null) {
-                // Update the DOM element with the compass direction value
-                document.getElementById('compassDirection').innerText = `Compass Direction: ${compassDirection}°`;
-            }
+        p.textContent = compassDirection;
+        document.body.appendChild(p);
         }
