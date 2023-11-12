bc2c6635f0fcce62a0c9ef95464f4bce7e6c8c29
bcebel <bcebel@gmail.com>
2023-11-11 19:12:44 -0500

    test

--- js/validate.js
+++ js/validate.js
@@ -21,47 +21,47 @@ document.addEventListener("DOMContentLoaded", function () {
     if (compassDirection > 337 || compassDirection <= 22) {
       photoCompass.setAttribute("src", "6.jpg");
       console.log(photoCompass);
-      window.location.href = "home.html#six";
+      window.location.href = "home.html#page=six";
     }
 
     if (compassDirection > 22 && compassDirection <= 67) {
       photoCompass.setAttribute("src", "5.jpg");
       console.log(photoCompass);
-      window.location.href = "home.html#five";
+      window.location.href = "home.html#page=five";
     }
 
     if (compassDirection > 67 && compassDirection <= 112) {
       photoCompass.setAttribute("src", "4.jpg");
       console.log(photoCompass);
-      window.location.href = "home.html#four";
+      window.location.href = "home.html#page=four";
     }
 
     if (compassDirection > 112 && compassDirection <= 157) {
       photoCompass.setAttribute("src", "3.jpg");
       console.log(photoCompass);
-      window.location.href = "home.html#three";
+      window.location.href = "home.html#page=three";
     }
 
     if (compassDirection > 157 && compassDirection <= 202) {
       photoCompass.setAttribute("src", "2.jpg");
       console.log(photoCompass);
-      window.location.href = "home.html#two";
+      window.location.href = "home.html#page=two";
     }
 
     if (compassDirection > 202 && compassDirection <= 247) {
       photoCompass.setAttribute("src", "1.jpg");
       console.log(photoCompass);
-      window.location.href = "home.html#one";
+      window.location.href = "home.html#page=one";
     }
     if (compassDirection > 247 && compassDirection <= 292) {
       photoCompass.setAttribute("src", "8.jpg");
       console.log(photoCompass);
-      window.location.href = "home.html#eight";
+      window.location.href = "home.html#page=eight";
     }
     if (compassDirection > 292 && compassDirection <= 337) {
       photoCompass.setAttribute("src", "7.jpg");
       console.log(photoCompass);
-      window.location.href = "home.html#seven";
+      window.location.href = "home.html#page=seven";
     }
   }
 });
