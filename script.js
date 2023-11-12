96d3315647c3e311db9dc84e86aa4c4bc0aede04
bcebel <bcebel@gmail.com>
2023-11-11 20:04:02 -0500

    devide

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ window.addEventListener("deviceorientationabsolute", handleOrientation);
 
 function handleOrientation(event) {
   //    const compassDirection = 360-event.alpha;
-  const compassDirection = 360 - event.alpha;
-  <template type="amp-mustache">{{ compassDirection }}</template>;
+  const compassDirection = event.alpha;
+ // <template type="amp-mustache">{{ compassDirection }}</template>;
   console.log(compassDirection);
-  if (compassDirection !== null) {
+ // if (compassDirection !== null) {
     // Update the DOM element with the compass direction value
-    document.getElementById("inputCompass").setAttribute =
-      (heading - end, compassDirection);
-    console.log(inputCompass.compassDirection);
+ //   document.getElementById("inputCompass").setAttribute =
+ //     (heading - end, compassDirection);
+   // console.log(inputCompass.compassDirection);
   }
-}
+//}
 
