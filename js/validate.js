8731613e955abd97321d9c16c4a2ba31d2baa3ab
bcebel <bcebel@gmail.com>
2023-12-03 21:25:01 -0500

    compass off one direction

--- js/validate.js
+++ js/validate.js
@@ -9,43 +9,33 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("wombats", compassDirection);
 
     if (compassDirection > 337 || compassDirection <= 22) {
-      window.open(
-        "https://bcebel.github.io/3d/home.html#page=five"
-      );
+      window.open("https://bcebel.github.io/3d/home.html#page=six");
     }
 
     if (compassDirection > 22 && compassDirection <= 67) {
-      window.open(
-        "https://bcebel.github.io/3d/home.html#page=four"
-      );
+      window.open("https://bcebel.github.io/3d/home.html#page=five");
     }
 
     if (compassDirection > 67 && compassDirection <= 112) {
-      window.open(
-        "https://bcebel.github.io/3d/home.html#page=three"
-      );
+      window.open("https://bcebel.github.io/3d/home.html#page=four");
     }
 
     if (compassDirection > 112 && compassDirection <= 157) {
-      window.open("https://bcebel.github.io/3d/home.html#page=two");
+      window.open("https://bcebel.github.io/3d/home.html#page=three");
     }
 
     if (compassDirection > 157 && compassDirection <= 202) {
-      window.open("https://bcebel.github.io/3d/home.html#page=one");
+      window.open("https://bcebel.github.io/3d/home.html#page=two");
     }
 
     if (compassDirection > 202 && compassDirection <= 247) {
-      window.open(
-        "https://bcebel.github.io/3d/home.html#page=eight"
-      );
+      window.open("https://bcebel.github.io/3d/home.html#page=one");
     }
     if (compassDirection > 247 && compassDirection <= 292) {
-      window.open(
-        "https://bcebel.github.io/3d/home.html#page=seven"
-      );
+      window.open("https://bcebel.github.io/3d/home.html#page=eight");
     }
     if (compassDirection > 292 && compassDirection <= 337) {
-      window.open("https://bcebel.github.io/3d/home.html#page=six");
+      window.open("https://bcebel.github.io/3d/home.html#page=seven");
     }
   }
 });
