cf8072b7a8d9100bd4a576a3de9c0540dda68a42
bcebel <bcebel@gmail.com>
2023-12-03 20:29:35 -0500

    got rid of _blank because it caused popups

--- js/validate.js
+++ js/validate.js
@@ -9,33 +9,43 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("wombats", compassDirection);
 
     if (compassDirection > 337 || compassDirection <= 22) {
-      window.open("https://bcebel.github.io/3d/home.html#page=five", "_blank");
+      window.open(
+        "https://bcebel.github.io/3d/home.html#page=five"
+      );
     }
 
     if (compassDirection > 22 && compassDirection <= 67) {
-      window.open("https://bcebel.github.io/3d/home.html#page=four", "_blank");
+      window.open(
+        "https://bcebel.github.io/3d/home.html#page=four"
+      );
     }
 
     if (compassDirection > 67 && compassDirection <= 112) {
-      window.open("https://bcebel.github.io/3d/home.html#page=three", "_blank");
+      window.open(
+        "https://bcebel.github.io/3d/home.html#page=three"
+      );
     }
 
     if (compassDirection > 112 && compassDirection <= 157) {
-      window.open("https://bcebel.github.io/3d/home.html#page=two", "_blank");
+      window.open("https://bcebel.github.io/3d/home.html#page=two");
     }
 
     if (compassDirection > 157 && compassDirection <= 202) {
-      window.open("https://bcebel.github.io/3d/home.html#page=one", "_blank");
+      window.open("https://bcebel.github.io/3d/home.html#page=one");
     }
 
     if (compassDirection > 202 && compassDirection <= 247) {
-      window.open("https://bcebel.github.io/3d/home.html#page=eight", "_blank");
+      window.open(
+        "https://bcebel.github.io/3d/home.html#page=eight"
+      );
     }
     if (compassDirection > 247 && compassDirection <= 292) {
-      window.open("https://bcebel.github.io/3d/home.html#page=seven", "_blank");
+      window.open(
+        "https://bcebel.github.io/3d/home.html#page=seven"
+      );
     }
     if (compassDirection > 292 && compassDirection <= 337) {
-      window.open("https://bcebel.github.io/3d/home.html#page=six", "_blank");
+      window.open("https://bcebel.github.io/3d/home.html#page=six");
     }
   }
 });
