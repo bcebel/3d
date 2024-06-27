c1ec907a3e94c9149bc2c0c0148745b7e863a7c7
bcebel <bcebel@gmail.com>
2024-06-26 19:12:07 -0500

    simplify

--- js/validate.js
+++ js/validate.js
@@ -1,49 +1,60 @@
 console.log("hello");
 console.log(document.body);
-const name = document.querySelector(".name");
+let num = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("deviceorientationabsolute", handleOrientation, true);
 
+  function handleOrientation(event) {
+    const compassDirection = event.alpha;
+    console.log("wombats", compassDirection);
 
-document.addEventListener("DOMContentLoaded", function myButton() {
-    
-    window.addEventListener(
-      "deviceorientationabsolute",
-      handleOrientation,
-      true
-    );
-
-
-    function handleOrientation(event) {
-      const compassDirection = event.alpha;
-      console.log("wombats", compassDirection);
-
-      if (compassDirection > 337 || compassDirection <= 22) {
-        window.open("https://bcebel.github.io/3d/home.html#page=six");
-      }
-
-      if (compassDirection > 22 && compassDirection <= 67) {
-        window.open("https://bcebel.github.io/3d/home.html#page=five");
-      }
-
-      if (compassDirection > 67 && compassDirection <= 112) {
-        window.open("https://bcebel.github.io/3d/home.html#page=four");
-      }
-
-      if (compassDirection > 112 && compassDirection <= 157) {
-        window.open("https://bcebel.github.io/3d/home.html#page=three");
-      }
-
-      if (compassDirection > 157 && compassDirection <= 202) {
-        window.open("https://bcebel.github.io/3d/home.html#page=two");
-      }
-
-      if (compassDirection > 202 && compassDirection <= 247) {
-        window.open("https://bcebel.github.io/3d/home.html#page=one");
-      }
-      if (compassDirection > 247 && compassDirection <= 292) {
-        window.open("https://bcebel.github.io/3d/home.html#page=eight");
-      }
-      if (compassDirection > 292 && compassDirection <= 337) {
-        window.open("https://bcebel.github.io/3d/home.html#page=seven");
-      }
+    if (compassDirection > 337 || compassDirection <= 22) {
+      num = 1;
     }
-  });
+
+    if (compassDirection > 22 && compassDirection <= 67) {
+      num = 2;
+    }
+
+    if (compassDirection > 67 && compassDirection <= 112) {
+      num = 3;
+    }
+
+    if (compassDirection > 112 && compassDirection <= 157) {
+      num = 4;
+    }
+
+    if (compassDirection > 157 && compassDirection <= 202) {
+      num = 5;
+    }
+
+    if (compassDirection > 202 && compassDirection <= 247) {
+      num = 6;
+    }
+    if (compassDirection > 247 && compassDirection <= 292) {
+      num = 7;
+    }
+    if (compassDirection > 292 && compassDirection <= 337) {
+      num = 8;
+    }
+  }
+});
+function myButton() {
+  if (num == 1) {
+    window.open("https://bcebel.github.io/3d/home.html#page=six");
+  } else if (num == 2) {
+    window.open("https://bcebel.github.io/3d/home.html#page=five");
+  } else if (num == 3) {
+    window.open("https://bcebel.github.io/3d/home.html#page=four");
+  } else if (num == 4) {
+    window.open("https://bcebel.github.io/3d/home.html#page=three");
+  } else if (num == 5) {
+    window.open("https://bcebel.github.io/3d/home.html#page=two");
+  } else if (num == 6) {
+    window.open("https://bcebel.github.io/3d/home.html#page=one");
+  } else if (num == 7) {
+    window.open("https://bcebel.github.io/3d/home.html#page=eight");
+  } else if (num == 8) {
+    window.open("https://bcebel.github.io/3d/home.html#page=seven");
+  }
+}
