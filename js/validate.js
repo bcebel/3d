9f9931b0bc489befc4ca1fa422b4ede5e67cd648
bcebel <bcebel@gmail.com>
2024-07-01 13:58:06 -0500

    refactor for ios

--- js/validate.js
+++ js/validate.js
@@ -1,8 +1,18 @@
 console.log("hello");
 console.log(document.body);
+
 let num = 0;
+
 document.addEventListener("DOMContentLoaded", function () {
-  window.addEventListener("deviceorientationabsolute", handleOrientation, true);
+  document.getElementById("myButton").addEventListener("click", myButton);
+
+  function myButton() {
+    window.addEventListener(
+      "deviceorientationabsolute",
+      handleOrientation,
+      true
+    );
+  }
 
   function handleOrientation(event) {
     const compassDirection = event.alpha;
@@ -10,51 +20,56 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (compassDirection > 337 || compassDirection <= 22) {
       num = 1;
-    }
-
-    if (compassDirection > 22 && compassDirection <= 67) {
+    } else if (compassDirection > 22 && compassDirection <= 67) {
       num = 2;
-    }
-
-    if (compassDirection > 67 && compassDirection <= 112) {
+    } else if (compassDirection > 67 && compassDirection <= 112) {
       num = 3;
-    }
-
-    if (compassDirection > 112 && compassDirection <= 157) {
+    } else if (compassDirection > 112 && compassDirection <= 157) {
       num = 4;
-    }
-
-    if (compassDirection > 157 && compassDirection <= 202) {
+    } else if (compassDirection > 157 && compassDirection <= 202) {
       num = 5;
+    } else if (compassDirection > 202 && compassDirection <= 247) {
+      num = 6;
+    } else if (compassDirection > 247 && compassDirection <= 292) {
+      num = 7;
+    } else if (compassDirection > 292 && compassDirection <= 337) {
+      num = 8;
     }
 
-    if (compassDirection > 202 && compassDirection <= 247) {
-      num = 6;
+    openPageBasedOnDirection(num);
+  }
+
+  function openPageBasedOnDirection(num) {
+    let url = "";
+    switch (num) {
+      case 1:
+        url = "https://bcebel.github.io/3d/home.html#page=six";
+        break;
+      case 2:
+        url = "https://bcebel.github.io/3d/home.html#page=five";
+        break;
+      case 3:
+        url = "https://bcebel.github.io/3d/home.html#page=four";
+        break;
+      case 4:
+        url = "https://bcebel.github.io/3d/home.html#page=three";
+        break;
+      case 5:
+        url = "https://bcebel.github.io/3d/home.html#page=two";
+        break;
+      case 6:
+        url = "https://bcebel.github.io/3d/home.html#page=one";
+        break;
+      case 7:
+        url = "https://bcebel.github.io/3d/home.html#page=eight";
+        break;
+      case 8:
+        url = "https://bcebel.github.io/3d/home.html#page=seven";
+        break;
     }
-    if (compassDirection > 247 && compassDirection <= 292) {
-      num = 7;
-    }
-    if (compassDirection > 292 && compassDirection <= 337) {
-      num = 8;
+
+    if (url) {
+      window.open(url);
     }
   }
 });
-function myButton() {
-  if (num == 1) {
-    window.open("https://bcebel.github.io/3d/home.html#page=six");
-  } else if (num == 2) {
-    window.open("https://bcebel.github.io/3d/home.html#page=five");
-  } else if (num == 3) {
-    window.open("https://bcebel.github.io/3d/home.html#page=four");
-  } else if (num == 4) {
-    window.open("https://bcebel.github.io/3d/home.html#page=three");
-  } else if (num == 5) {
-    window.open("https://bcebel.github.io/3d/home.html#page=two");
-  } else if (num == 6) {
-    window.open("https://bcebel.github.io/3d/home.html#page=one");
-  } else if (num == 7) {
-    window.open("https://bcebel.github.io/3d/home.html#page=eight");
-  } else if (num == 8) {
-    window.open("https://bcebel.github.io/3d/home.html#page=seven");
-  }
-}
