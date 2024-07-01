ec6f4339253181a96466a867d592234def6f89d5
bcebel <bcebel@gmail.com>
2024-07-01 18:04:57 -0500

    tina eat your ham

--- js/validate.js
+++ js/validate.js
@@ -1,10 +1,10 @@
 console.log("hello");
 console.log(document.body);
 let num = 0;
-document.getElementById("myButton").addEventListener("click", myButton());
+ document.getElementById("myButton").addEventListener("click", myButton);
 document.addEventListener("DOMContentLoaded", function () {
-window.addEventListener("deviceorientationabsolute", handleOrientation, true);
-
+  window.addEventListener("deviceorientationabsolute", handleOrientation, true);
+  
  
 
 
@@ -44,22 +44,25 @@ window.addEventListener("deviceorientationabsolute", handleOrientation, true);
   }
 });
 
+
 function myButton() {
-  if (num == 1) {
-    window.open("https://bcebel.github.io/3d/home.html#page=six");
-  } else if (num == 2) {
-    window.open("https://bcebel.github.io/3d/home.html#page=five");
-  } else if (num == 3) {
-    window.open("https://bcebel.github.io/3d/home.html#page=four");
-  } else if (num == 4) {
-    window.open("https://bcebel.github.io/3d/home.html#page=three");
-  } else if (num == 5) {
-    window.open("https://bcebel.github.io/3d/home.html#page=two");
-  } else if (num == 6) {
-    window.open("https://bcebel.github.io/3d/home.html#page=one");
-  } else if (num == 7) {
-    window.open("https://bcebel.github.io/3d/home.html#page=eight");
-  } else if (num == 8) {
-    window.open("https://bcebel.github.io/3d/home.html#page=seven");
+  setTimeout(() => {
+    if (num == 1) {
+      window.open("./home.html#page=six");
+    } else if (num == 2) {
+      window.open("./home.html#page=five");
+    } else if (num == 3) {
+      window.open("./home.html#page=four");
+    } else if (num == 4) {
+      window.open("./home.html#page=three");
+    } else if (num == 5) {
+      window.open("./home.html#page=two");
+    } else if (num == 6) {
+      window.open("./home.html#page=one");
+    } else if (num == 7) {
+      window.open("./home.html#page=eight");
+    } else if (num == 8) {
+      window.open("./home.html#page=seven");
+    }
+  });
   }
-}
